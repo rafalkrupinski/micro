4c042243c28c7fc3e9bf09f7fedf0e83d3892302
Rafał Krupiński <rkrupinski+github@gmail.com>
2016-01-02 10:39:31 +0000

    [compare] Remove max() available from Guava, add comparsion operators

--- micro-compare/src/main/java/com/hashnot/u/compare/Comparables.java
+++ micro-compare/src/main/java/com/hashnot/u/compare/Comparables.java
@@ -5,7 +5,38 @@ package com.hashnot.u.compare;
  */
 public class Comparables {
 
-    public static <T extends Comparable<T>> T max(T a, T b) {
-        return a.compareTo(b) >= 0 ? a : b;
+    /**
+     * Allows parameters to be null
+     */
+    public static <T extends Comparable<T>> boolean eq(T o1, T o2) {
+        return o1 == o2 || o1 != null && o2 != null && o1.compareTo(o2) == 0;
     }
+
+    /**
+     * Allows parameters to be null
+     */
+    public static <T extends Comparable<T>> boolean ne(T o1, T o2) {
+        return o1 != o2 || o1 == null || o1.compareTo(o2) != 0;
+    }
+
+    public static <T extends Comparable<T>> boolean lt(T o1, T o2) {
+        return o1.compareTo(o2) < 0;
+    }
+
+    public static <T extends Comparable<T>> boolean lte(T o1, T o2) {
+        return o1.compareTo(o2) <= 0;
+    }
+
+    public static <T extends Comparable<T>> boolean gt(T o1, T o2) {
+        return o1.compareTo(o2) > 0;
+    }
+
+    /**
+     * @return o1 >= o2
+     */
+    public static <T extends Comparable<T>> boolean gte(T o1, T o2) {
+        return o1.compareTo(o2) >= 0;
+    }
+
+
 }
--- micro-ratelimit/src/main/java/com/hashnot/u/ratelimit/RateLimiter.java
+++ micro-ratelimit/src/main/java/com/hashnot/u/ratelimit/RateLimiter.java
@@ -1,6 +1,6 @@
 package com.hashnot.u.ratelimit;
 
-import com.hashnot.u.compare.Comparables;
+import com.google.common.collect.Ordering;
 
 import java.time.Clock;
 import java.time.Duration;
@@ -17,6 +17,7 @@ import static java.time.Duration.ZERO;
  * @author Rafał Krupiński
  */
 public class RateLimiter implements IRateLimiter {
+    final private static Ordering<Comparable> CMP = Ordering.natural();
     final private Clock clock;
 
     final private Map<RateLimit, RateLimitState> state = new HashMap<>();
@@ -45,7 +46,7 @@ public class RateLimiter implements IRateLimiter {
 
             for (Map.Entry<RateLimit, RateLimitState> e : state.entrySet()) {
                 RateLimitState rateLimitState = e.getValue();
-                sleep = Comparables.max(sleep, rateLimitState.waitTimeForPermit(actual, acquire));
+                sleep = CMP.max(sleep, rateLimitState.waitTimeForPermit(actual, acquire));
                 rateLimitState.add(actual, acquire);
             }
 
@@ -63,7 +64,7 @@ public class RateLimiter implements IRateLimiter {
             Duration sleep = Duration.ofSeconds(Long.MIN_VALUE);
 
             for (Map.Entry<RateLimit, RateLimitState> e : state.entrySet()) {
-                sleep = Comparables.max(sleep, e.getValue().waitTimeForPermit(actual, acquire));
+                sleep = CMP.max(sleep, e.getValue().waitTimeForPermit(actual, acquire));
             }
 
             if (sleep.compareTo(ZERO) > 0)
