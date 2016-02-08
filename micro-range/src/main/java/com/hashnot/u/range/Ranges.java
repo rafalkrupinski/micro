7c8e35cf9de324e2962cd13c07e0a0a862768814
Rafał Krupiński <rkrupinski+github@gmail.com>
2016-02-08 18:28:06 +0000

    [range] Add Ranges.isSingleton()

--- micro-range/src/main/java/com/hashnot/u/range/Ranges.java
+++ micro-range/src/main/java/com/hashnot/u/range/Ranges.java
@@ -1,5 +1,6 @@
 package com.hashnot.u.range;
 
+import com.google.common.collect.BoundType;
 import com.google.common.collect.Range;
 
 import java.time.Duration;
@@ -9,10 +10,17 @@ import java.time.temporal.Temporal;
  * @author Rafał Krupiński
  */
 public class Ranges {
-    protected Ranges() {
+    private Ranges() {
+        throw new RuntimeException("Util class");
     }
 
     public static <ComparableT extends Comparable<ComparableT>, TemporalT extends Temporal & Comparable<ComparableT>> Duration between(Range<TemporalT> timeRange) {
         return Duration.between(timeRange.lowerEndpoint(), timeRange.upperEndpoint());
+    }
+
+    public static <T extends Comparable<T>> boolean isSingleton(Range<T> range) {
+        return range.upperBoundType() == BoundType.CLOSED
+                && range.lowerBoundType() == BoundType.CLOSED
+                && range.lowerEndpoint().equals(range.upperEndpoint());
     }
 }
